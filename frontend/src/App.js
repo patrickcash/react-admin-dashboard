68197b4040e02ef385947af8bb7c5b66d5dcc2cd
Patrick Cash <patrickcash@gmail.com>
2022-03-05 16:49:11 -0500

    added some icons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 import {Admin, Resource} from 'react-admin'
 import jsonServerProvider from 'ra-data-json-server'
+import PostIcon from '@material-ui/icons/Book';
+import UserIcon from '@material-ui/icons/People';
 
 import PostCreate from './components/post/PostCreate';
 import PostEdit from './components/post/PostEdit';
@@ -15,12 +17,14 @@ function App() {
     <Admin dataProvider={jsonServerProvider('http://localhost:3000')}>
       <Resource 
         name='posts'
+        icon={PostIcon}
         create={PostCreate}
         edit={PostEdit}
         list={PostList}
       />
       <Resource 
         name='users'
+        icon={UserIcon}
         create={UserCreate}
         edit={UserEdit}
         list={UserList}
